1fef0c70e1a662fda0c9eed1036be16b0e3a6280
David Durkin <dldurks@gmail.com>
2017-04-19 10:48:26 -0400

    Added my phocations feature

---
+++ MyPhocationViewer.swift
@@ -0,0 +1,60 @@
+//
+//  MyPhocationViewer.swift
+//  Phocation
+//
+//  Created by David Durkin on 4/19/17.
+//  Copyright © 2017 David Durkin. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import UIKit
+import Parse
+import Bolts
+
+extension MyPhocationsViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is Phocation {
+            let annotation = annotation as! Phocation
+            var view: MKAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: annotation.title!) { // 2
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                // 3
+                view = MKAnnotationView(annotation: annotation,reuseIdentifier: annotation.title)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+            }
+            view.isEnabled = true
+            
+            // Resize image
+            let pinImage = UIImage(named: "redCircle.png")
+            let size = CGSize(width: 25, height: 25)
+            UIGraphicsBeginImageContext(size)
+            pinImage!.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            view.image = resizedImage
+            return view
+        }
+        return nil
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            super.performSegue(withIdentifier: "phocationSegue", sender: view)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "phocationSegue"){
+            let phocationViewController = (segue.destination as! PhocationViewController)
+            phocationViewController.id = (sender as! MKAnnotationView).annotation!.title!
+        }
+    }
+}
---
+++ MyPhocationsViewController.swift
@@ -0,0 +1,91 @@
+//
+//  MyPhocationsViewController.swift
+//  Phocation
+//
+//  Created by David Durkin on 4/19/17.
+//  Copyright © 2017 David Durkin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+import Bolts
+import MapKit
+import CoreLocation
+
+class MyPhocationsViewController: UIViewController, CLLocationManagerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    let locationManager = CLLocationManager()
+    
+    var posts = NSMutableArray()
+    
+    var drawer = PhocationAnnotationView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        //mapView.showsUserLocation = true
+        
+        let phocations = PFQuery(className: "Post")
+        phocations.whereKey("userName", equalTo: currentUser.userName! as String)
+        phocations.findObjectsInBackground {
+            (objects: [PFObject]?, error: Error?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) posts.")
+                // Do something with the found objects
+                if let objects = objects {
+                    for object in objects {
+                        //self.posts.add(object["userLocation"])
+                        let lat = object["userLat"] as! String
+                        let long = object["userLong"] as! String
+                        let id = object.objectId as String!
+                        let user = object["userName"] as! String
+                        let user_id = user + ":" + id!
+                        print("\(user_id)")
+                        let longD = Double(long), latD = Double(lat)
+                        print("\(lat) \(long) \(id)")
+                        let pinLocation : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latD!, longD!)
+                        let objectAnnotation = Phocation(id: id!, user: user, coordinate: pinLocation)
+                        self.mapView.addAnnotation(objectAnnotation)
+                    }
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) ")
+            }
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+        currentUser.latitude = String(format: "%f", (location?.coordinate.latitude)!)
+        currentUser.longitude = String(format: "%f", (location?.coordinate.longitude)!)
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error!: " + error.localizedDescription)
+    }
+}
